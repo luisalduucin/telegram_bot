932c8f8f502ca77b52468c0c0408e4ce55ab9a4a
Luis Alducin <luisalduucin@users.noreply.github.com>
2017-03-02 19:06:53 -0600

    Added diferent operation modes

--- Main.py
+++ Main.py
@@ -3,15 +3,36 @@ import time
 import telepot
 import os
 
+token = os.getenv('TELEGRAM_ACCESS_TOKEN', '')
+users = {}
+
+def setOperationMode(operationMode, chat_id):
+    if operationMode.startswith('/') and operationMode != '/change_mode':
+        users[chat_id] = operationMode
+        bot.sendMessage(chat_id, 'Modo de operacion ' + operationMode + ' activado')
+    else:
+        bot.sendMessage(chat_id, 'Selecciona un modo de operacion')
+
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    print(content_type, chat_type, chat_id)
-
+    mensaje = msg['text']
     if content_type == 'text':
-        bot.sendMessage(chat_id, msg['text'])
-
-#TOKEN = sys.argv[1]  # get token from command-line
-token = os.getenv('TELEGRAM_ACCESS_TOKEN', '')
+        if chat_id not in users:
+            setOperationMode(mensaje, chat_id)
+        else:
+            if msg['text'] == '/change_mode':
+                users.pop(chat_id, None)
+                setOperationMode(mensaje, chat_id)
+            elif users[chat_id] == '/echo_mode':
+                bot.sendMessage(chat_id, mensaje)
+            elif users[chat_id] == '/debug_mode':
+                print(content_type, chat_type, chat_id)
+            elif users[chat_id] == '/witai_mode':
+                bot.sendMessage(chat_id, 'En desarrollo')
+            else:
+                bot.sendMessage(chat_id, 'Ese modo de operacion no es valido')
+    else:
+        bot.sendMessage(chat_id, 'Ese modo tipo de mensaje no es valido')
 
 if token == '':
 	print('Set TELEGRAM_ACCESS_TOKEN env variable')
@@ -21,6 +42,5 @@ bot = telepot.Bot(token)
 bot.message_loop(handle)
 print ('Listening ...')
 
-# Keep the program running.
 while 1:
     time.sleep(10)
