eabca3829bf8c6746dd28367dcd66e0e9ca9ca25
luisalduucin <lalducin@nearsoft.com>
2017-08-29 17:00:26 -0500

    Added logger

--- example_un_amigo_bot/telegram_bot_impl.py
+++ example_un_amigo_bot/telegram_bot_impl.py
@@ -1,3 +1,6 @@
+import logging
+import pprint
+
 from example_un_amigo_bot.helpers.custom_keyboard_provider import CustomKeyboardProvider
 from example_un_amigo_bot.helpers.questions_provider import QuestionsProvider
 from telegram_api.telegram_bot import TelegramBot
@@ -12,10 +15,11 @@ class TelegramBotImpl(TelegramBot):
         self.wit_client = WitAiClient(wit_ai_access_token)
         self.questions_repository = QuestionsProvider()
         self.custom_keyboard_repository = CustomKeyboardProvider()
-        self.current_question = {}
+        self.current_question_index = {}
+        self.depression_profile = {}
 
     def get_question_with_custom_keyboard(self, chat_id):
-        question_index = self.current_question[chat_id]
+        question_index = self.current_question_index[chat_id]
         question = self.questions_repository.provide(question_index)
         custom_keyboard = self.custom_keyboard_repository.provide(question)
         return question, custom_keyboard
@@ -27,22 +31,27 @@ class TelegramBotImpl(TelegramBot):
     def handler(self, msg):
         content_type, chat_type, chat_id = TelegramBot.glance(msg)
         message = msg['text']
-
         if content_type == 'text':
-
-            if chat_id not in self.current_question and START_QUESTIONARY_SIGNAL in self.wit_client.get_response(chat_id).decode('utf-8'):
-                # TODO guardar como se describe a si mismo
-                self.current_question[chat_id] = 0
-                self.send_current_question(chat_id)
-            elif chat_id in self.current_question:
-                self.current_question[chat_id] += 1
-                if self.questions_repository.are_remaining_questions(self.current_question[chat_id]):
-                    self.send_current_question(chat_id)
-                else:
-                    self.current_question.pop(chat_id, None)
-                    self.send_wit_ai_response(chat_id)
+            if message == '/start':
+                self.send_wit_ai_response(chat_id, 'Hola')
+            elif message == '/report':
+                self.send_statistics()
             else:
-                self.send_wit_ai_response(chat_id, message)
+                if chat_id not in self.current_question_index and \
+                                START_QUESTIONARY_SIGNAL in self.wit_client.get_response(chat_id).decode('utf-8'):
+                    self.save_self_description(chat_id, message)
+                    self.current_question_index[chat_id] = 0
+                    self.send_current_question(chat_id)
+                elif chat_id in self.current_question_index:
+                    self.current_question_index[chat_id] += 1
+                    if self.questions_repository.are_remaining_questions(self.current_question_index[chat_id]):
+                        self.send_current_question(chat_id)
+                        self.update_depression_score(chat_id, response=message)
+                    else:
+                        self.current_question_index.pop(chat_id, None)
+                        self.send_wit_ai_response(chat_id)
+                else:
+                    self.send_wit_ai_response(chat_id, message)
 
         else:
             self.send(chat_id, 'Ese tipo de message no es valido')
@@ -50,3 +59,22 @@ class TelegramBotImpl(TelegramBot):
     def send_current_question(self, chat_id):
         question, custom_keyboard = self.get_question_with_custom_keyboard(chat_id)
         self.send_with_custom_keyboard(chat_id, question, custom_keyboard)
+
+    def save_self_description(self, chat_id, description):
+        if chat_id not in self.depression_profile:
+            self.depression_profile[chat_id] = {}
+        self.depression_profile['SELF_DESCRIPTION'] = description
+    
+    def update_depression_score(self, chat_id, response):
+        question_index = self.current_question_index[chat_id]
+        question = self.questions_repository.provide(question_index)
+        button_value = self.custom_keyboard_repository.get_button_value(question, response)
+        if chat_id not in self.depression_profile:
+            self.depression_profile[chat_id] = {}
+            if 'DEPRESSION_SCORE' not in self.depression_profile[chat_id]:
+                self.depression_profile['DEPRESSION_SCORE'] = 0
+        self.depression_profile['DEPRESSION_SCORE'] += button_value
+
+    def send_statistics(self):
+        logging.info('\n\nUsers Statistics:\n\n'
+                     + '-> User Profiles:\n\n' + pprint.pformat(self.depression_profile) + '\n\n')
